57e121c7abca8f4672e7ae84e25b920750b31ede
amarpimpale98 <amar.pimpale@humancloud.co.in>
2024-10-26 11:58:34 +0530

    test for combination of custom delimiters

--- tddCalculator.ts
+++ tddCalculator.ts
@@ -1,7 +1,16 @@
 export function add(numbers: string): number {
   if (numbers === "") return 0; 
 
-  const delimiter = /,|\n/; // Handle commas and newlines as delimiters
+  let delimiter = /,|\n/; 
+
+  if (numbers.startsWith("//")) {
+    const delimiterEnd = numbers.indexOf("\n");
+    const customDelimiter = numbers.substring(2, delimiterEnd);
+    delimiter = new RegExp(
+      customDelimiter.replace(/[.*+?^${}()|[\]\\]/g, "\\$&")
+    ); 
+    numbers = numbers.substring(delimiterEnd + 1);
+  }
   const numArray = numbers.split(delimiter);
   return numArray.reduce((sum, num) => sum + parseInt(num, 10), 0);
 }
