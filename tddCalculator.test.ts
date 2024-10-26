957afaba7c746d335ec3874df7bfe324c6f06096
amarpimpale98 <amar.pimpale@humancloud.co.in>
2024-10-26 11:45:48 +0530

    handle single number as input

--- tddCalculator.test.ts
+++ tddCalculator.test.ts
@@ -3,3 +3,7 @@
     test("Should return 0 for empty string",()=>{
         expect(add("")).toBe(0)
     })
+
+    test("should return the number itself for a single number", () => {
+      expect(add("1")).toBe(1);
+    });
